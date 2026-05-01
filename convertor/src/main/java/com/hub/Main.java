2126f078e27fab57776e4cfacd19d53b26fe82ae
Dawood Sardar <dawoodsardar252@gmail.com>
2026-05-01 18:30:24 +0500

    Setup Maven, JavaFx and corrected pom file.

--- convertor/src/main/java/com/hub/Main.java
+++ convertor/src/main/java/com/hub/Main.java
@@ -1,7 +1,26 @@
 package com.hub;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Convertor app is running");
+
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 900, 600);
+
+        stage.setTitle("ConverterHubPro");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch();
     }
 }
