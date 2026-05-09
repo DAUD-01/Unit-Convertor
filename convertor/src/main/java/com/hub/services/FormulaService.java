b6d35ad3483de94a5ae8e8609e2d53ff1a590824
Dawood Sardar <dawoodsardar252@gmail.com>
2026-05-09 14:57:35 +0500

    Add FormulaService class for unit calculation logic with error handling

---
+++ convertor/src/main/java/com/hub/services/FormulaService.java
@@ -0,0 +1,35 @@
+package com.hub.services;
+
+import com.hub.core.FormulaEngine;
+
+public class FormulaService {
+
+    private final FormulaEngine engine = new FormulaEngine();
+
+    public double calculate(String type, double... inputs) {
+
+        switch (type.toLowerCase()) {
+
+            case "bmi":
+                return engine.bmi(inputs[0], inputs[1]);
+
+            case "calories":
+                return engine.calories(inputs[0], inputs[1], inputs[2]);
+
+            case "simpleinterest":
+                return engine.simpleInterest(inputs[0], inputs[1], inputs[2]);
+
+            case "compoundinterest":
+                return engine.compoundInterest(inputs[0], inputs[1], inputs[2], inputs[3]);
+
+            case "roi":
+                return engine.roi(inputs[0], inputs[1]);
+
+            case "tax":
+                return engine.tax(inputs[0], inputs[1]);
+
+            default:
+                throw new IllegalArgumentException("Unknown formula type: " + type);
+        }
+    }
+}
