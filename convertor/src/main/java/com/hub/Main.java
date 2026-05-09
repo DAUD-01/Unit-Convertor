6ece78f4b62668281540c166bd1336126b6168a8
Dawood Sardar <dawoodsardar252@gmail.com>
2026-05-09 15:16:50 +0500

    Refactor Main class to streamline application startup and update FXML loading; remove unused code and improve readability

--- convertor/src/main/java/com/hub/Main.java
+++ convertor/src/main/java/com/hub/Main.java
@@ -1,71 +1,27 @@
 package com.hub;
 
-import com.hub.core.ConversionEngine;
-import com.hub.services.InputParserService;
-import com.hub.utils.FileLoader;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        System.out.println("Mic check 123");
+    public void start(Stage stage) throws Exception {
 
-        // var data = com.hub.utils.FileLoader.loadData("units.json");
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/dashboard.fxml")
+        );
 
-        // if (data != null && data.Science != null) {
+        Scene scene = new Scene(loader.load(), 900, 600);
 
-        // System.out.println("=== SCIENCE CATEGORY ===");
-
-        // for (String key : data.Science.keySet()) {
-        // System.out.println("SubCategory: " + key);
-
-        // var sub = data.Science.get(key);
-
-        // if (sub.units != null) {
-        // for (String unit : sub.units.keySet()) {
-        // System.out.println(" - " + unit + " = " + sub.units.get(unit));
-        // }
-        // }
-        // }
-        // }
-
-        Label label = new Label("Convertor app is running");
-
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 900, 600);
-
-        stage.setTitle("Converter+");
+        stage.setTitle("ConverterHub Pro");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-
-        var data = FileLoader.loadData("units.json");
-
-        var length = data.Common.get("Length");
-
-        double result = ConversionEngine.convert(
-                100,
-                "m",
-                "km",
-                length);
-
-        System.out.println("Result: " + result);
-
-        var input1 = InputParserService.parse("100m");
-        System.out.println(input1.value + " " + input1.unit);
-
-        var input2 = InputParserService.parse("250 kg");
-        System.out.println(input2.value + " " + input2.unit);
-
-        launch();
-
+        launch(args);
     }
 }
