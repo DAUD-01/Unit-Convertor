0cc1d0744698c90cd484cb1e57730ddf14b778a6
Dawood Sardar <dawoodsardar252@gmail.com>
2026-05-16 10:39:35 +0500

    Refactor Category class: Remove unused constructors and add inputs list for enhanced functionality

--- convertor/src/main/java/com/hub/models/Category.java
+++ convertor/src/main/java/com/hub/models/Category.java
@@ -1,18 +1,11 @@
 package com.hub.models;
 
 import java.util.Map;
+import java.util.List;
 
 public class Category {
     public String type;
     public String base;
     public Map<String, Double> units;
-
-    public Category() {
-    }
-
-    public Category(String type, String base, Map<String, Double> units) {
-        this.type = type;
-        this.base = base;
-        this.units = units;
-    }
+    public List<String> inputs; 
 }
