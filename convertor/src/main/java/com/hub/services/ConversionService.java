c386460b154b2d4db4e9a6de82885d14a94539ed
Dawood Sardar <dawoodsardar252@gmail.com>
2026-05-21 19:00:13 +0500

    Remove unused ConversionEngine instantiation and related code in ConversionService

--- convertor/src/main/java/com/hub/services/ConversionService.java
+++ convertor/src/main/java/com/hub/services/ConversionService.java
@@ -4,11 +4,11 @@ import com.hub.core.ConversionEngine;
 import com.hub.models.Category;
 
 public class ConversionService {
-    private final ConversionEngine engine;
+    // private final ConversionEngine engine;
 
-    public ConversionService() {
-        this.engine = new ConversionEngine();
-    }
+    // public ConversionService() {
+    //     this.engine = new ConversionEngine();
+    // }
 
     public double convert(double value, String from, String to, Category category) {
         return ConversionEngine.convert(value, to, from, category);
