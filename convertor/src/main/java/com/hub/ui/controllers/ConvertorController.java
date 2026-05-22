6c9b4dc231a8cd1041ed9540c0e07d1f1e1d5d49
Dawood Sardar <dawoodsardar252@gmail.com>
2026-05-22 19:43:54 +0500

    Fix import order in ConvertorController for consistency

--- convertor/src/main/java/com/hub/ui/controllers/ConvertorController.java
+++ convertor/src/main/java/com/hub/ui/controllers/ConvertorController.java
@@ -1,10 +1,11 @@
 package com.hub.ui.controllers;
 
 import com.hub.core.ConversionEngine;
-import com.hub.models.Category;
 import com.hub.services.AlgorithmService;
 import com.hub.services.FormulaService;
+import com.hub.models.Category;
 import com.hub.ui.utils.FXAnimation;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
