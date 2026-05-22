a436e6c1a2718cf89f26c91a7f365cb95b868314
Dawood Sardar <dawoodsardar252@gmail.com>
2026-05-22 21:53:47 +0500

    Add icon for the app

--- convertor/src/main/java/com/hub/Main.java
+++ convertor/src/main/java/com/hub/Main.java
@@ -3,9 +3,13 @@ package com.hub;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import java.io.InputStream;
 
-public class Main extends Application { // this is the main class that launches the application, it extends the Application class from JavaFX which provides the necessary methods to create and manage a JavaFX application
+public class Main extends Application { // this is the main class that launches the application, it extends the
+                                        // Application class from JavaFX which provides the necessary methods to create
+                                        // and manage a JavaFX application
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -20,6 +24,18 @@ public class Main extends Application { // this is the main class that launches 
         stage.setTitle("Convertor+");
         stage.setScene(scene);
         stage.setFullScreen(true);
+
+        // --- ADD APPLICATION WINDOW ICON ---
+        try (InputStream iconStream = getClass().getResourceAsStream("/assets/icon.png")) {
+            if (iconStream != null) {
+                stage.getIcons().add(new Image(iconStream));
+            } else {
+                System.err.println("Warning: Icon file not found at /src/main/resources/assets/icon.png");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         stage.show();
     }
 
